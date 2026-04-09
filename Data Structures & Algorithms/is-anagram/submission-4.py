bbc8457fcd768310bf5cd14ddf587292cd1efaa0
Dhruv Prajapati <dhruvprj1001@gmail.com>
2026-04-09 11:46:25 -0600

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        start = 0
+        end = len(s)-1
+
+        stack = []
+        values = {}
+
+        for i in s:
+            values[i] = i
+
+            if i in t and values[i]:
+                pass
+               
+            else: 
+                stack.append(1)
+
+        if len(stack) == 0:
+            return True
+
+        else:
+            return False
+            
